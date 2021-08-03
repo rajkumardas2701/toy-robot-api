a830d1f570cd0e28f7aac8970e0ebbc5b274ffc1
Rajkumar Das <rajkumardas2701@gmail.com>
2021-08-04 00:20:52 +0530

    Add logic to move newly placed Robot in all directions

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -3,19 +3,103 @@ class Robot < ApplicationRecord
   validates :y, presence: true
   validates :facing, presence: true
 
-  def check_move
-  end
+  # orientation = ['WEST', 'NORTH', 'EAST', 'SOUTH']
+  # orientation = ["WEST", "NORTH", "EAST", "SOUTH"]
   
+  def make_move(orient, out)
+    case orient
+    when "NORTH"
+      if out.y == 4
+        return "cannot move in this direction"
+      else
+        return [
+                out.x, out.y + 1, out.facing
+              ]
+      end
+    when "SOUTH"
+      if out.y == 0
+        return "cannot move in this direction"
+      else
+        return [
+                out.x, out.y - 1, out.facing
+              ]
+      end
+    when "EAST"
+      if out.x == 4
+        return "cannot move in this direction"
+      else
+        return [
+                out.x + 1, out.y, out.facing
+              ]
+      end
+    when "WEST"
+      if out.x == 0
+        return "cannot move in this direction"
+      else
+        return [
+                out.x - 1, out.y, out.facing
+              ]
+      end
+    else
+      "Invalid parameters"
+    end
+  end
+
+  def check_move(move, orient, out)
+    orientation = ["WEST", "NORTH", "EAST", "SOUTH"]
+    i = orientation.index(orient)
+    m = Robot.first
+    # byebug
+    case move
+    when "LEFT"
+      if i == 0
+        m.facing = orientation[3]
+        m.save
+        return [
+                out.x, out.y, out.facing = orientation[3]
+              ]
+      end
+      m.facing = orientation[i-1]
+      m.save
+      [
+        out.x, out.y, out.facing = orientation[i-1]
+      ]
+    when "RIGHT"
+      if i == 3
+        m.facing = orientation[0]
+        m.save
+        return [
+                out.x, out.y, out.facing = orientation[0]
+              ]
+      end
+      m.facing = orientation[i+1]
+      m.save
+      [
+        out.x, out.y, out.facing = orientation[i+1]
+      ]
+    when "MOVE"
+      out = make_move(orient, out)
+      m.save
+      return out
+    else
+      return "Invalid parameters"
+    end
+  end
+
   def create_robot(params)
     res = params[:commands].split(" ")
     cod = params[:commands].split(" ")[1]
+    # move = params[:commands].split(" ")[2].gsub(/\"/, '').gsub(/]/, '')
     # byebug
     if res.length > 2
+      move = params[:commands].split(" ")[2].gsub(/\"/, '').gsub(/]/, '')
       out = Robot.create(x: cod.split(",")[0].to_i,
                     y: cod.split(",")[1].to_i,
-                    facing: cod.split(",")[2].gsub!(/\"/, '')
+                    facing: cod.split(",")[2].gsub(/\"/, '')
       )
-      return [out.x, out.y, out.facing]
+      # byebug
+      return out = check_move(move, cod.split(",")[2].gsub(/\"/, ''), out)
+      # return out
       # byebug
     end
     return "you didn't place Robot on the table yet. Use PLACE keyword"
